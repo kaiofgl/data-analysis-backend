2adbe060dff31e9afdceb5c2a1ecc39291ce6a34
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-03-08 22:30:14 -0300

    Implementado ROOT_DIR_OUTPUT

--- app/config.py
+++ app/config.py
@@ -5,4 +5,5 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR_OUTPUT = ROOT_DIR + "/tmp/"
 DEBUG = os.environ.get("DEBUG")
