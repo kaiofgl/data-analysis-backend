6c1ddd1f3d5552ca595c80b5bb02a146512a3fd3
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-03-09 21:04:02 -0300

    Método de upload implementado

--- app/api/resources/file.py
+++ app/api/resources/file.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 
 # Services
 from api.services.file import FileService
@@ -9,13 +9,17 @@ FileService = FileService()
 
 @file_blueprint.route('upload', methods=['POST'])
 def upload():
-    return "", 200
-    # file = request.files['file']
-    # response = uploadFile(file);
-    # if(response):
-    #     return "", 200
-    # else:
-    #     return "", 400
+    file = request.files['file']
+    filename = request.form["name"]
+
+    result = FileService.upload(file, filename)
+
+    if not result:
+        return jsonify("An error has ocurred"), 404
+
+    response = make_response(result)
+    response.headers['Content-Type'] = 'application/json'
+    return response, 200
 
 @file_blueprint.route('get', methods=['POST'])
 def get():
--- app/api/services/file.py
+++ app/api/services/file.py
@@ -1,7 +1,24 @@
 import config as Config
 
-import json
+import os, json
+
+import pandas as pd
+
 class FileService:
+    @staticmethod
+    def upload(file, filename):
+        try:
+            fileReaded = pd.read_excel(file)
+            json = fileReaded.to_json(orient='records')
+
+            if not os.path.isdir(Config.ROOT_DIR_OUTPUT):
+                os.mkdir(Config.ROOT_DIR_OUTPUT)
+            with open(Config.ROOT_DIR_OUTPUT + "/" + filename + ".json", 'w') as fileOpened:
+                fileOpened.write(json)
+
+            return "uploaded"
+        except:
+            return False
     @staticmethod
     def getFile(filename):
         try:
